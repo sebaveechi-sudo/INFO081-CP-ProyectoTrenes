a3923d07a35abc06d39682189a0b2886a85ae1c4
krisiiis <cristox100000@gmail.com>
2025-11-09 16:03:56 -0300

    Add window dimensions and color settings

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# config/settings.py
+
+# Dimensiones de ventana
+ANCHO_VENTANA = 1024
+ALTO_VENTANA = 768
+
+# Colores (RGB)
+COLOR_FONDO = (240, 240, 240)
+COLOR_TEXTO = (0, 0, 0)
+COLOR_PRINCIPAL = (0, 122, 204)
