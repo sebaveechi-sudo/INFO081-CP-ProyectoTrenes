14eb97e7fcfef6a0144588267408c0df39dd27fb
krisiiis <cristox100000@gmail.com>
2025-11-29 20:40:48 -0300

    Refactor Tren class and add Pasajero and Estacion classes

--- models/tren.py
+++ models/tren.py
@@ -1,13 +1,46 @@
-# models/tren.py
+# models/entidades.py
+
+class Pasajero:
+    def __init__(self, id_p, origen_id, destino_id, hora_llegada_estacion):
+        self.id = id_p
+        self.origen_id = origen_id
+        self.destino_id = destino_id
+        self.hora_llegada_estacion = hora_llegada_estacion
+        self.esperando_desde = hora_llegada_estacion
+
+class Estacion:
+    def __init__(self, id_est, nombre, km_ubicacion):
+        self.id = id_est
+        self.nombre = nombre
+        self.km = km_ubicacion  # Kilómetro exacto según Anexo 1
+        self.cola_pasajeros = [] # Lista de Pasajeros esperando
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "km": self.km,
+            "pasajeros_esperando": len(self.cola_pasajeros)
+        }
 
 class Tren:
-    def __init__(self, id_tren, modelo, capacidad_maxima, velocidad):
-        self.id_tren = id_tren
+    def __init__(self, id_tren, modelo, capacidad, velocidad_max, km_actual=0):
+        self.id = id_tren
         self.modelo = modelo
-        self.capacidad_maxima = capacidad_maxima
-        self.velocidad = velocidad
-        self.vagones = []
-        self.estado = "EN_ESPERA"
+        self.capacidad = capacidad
+        self.velocidad_max = velocidad_max
+        self.km_actual = km_actual 
+        self.direccion = 1  # 1: Hacia el Sur (Chillán), -1: Hacia el Norte (Santiago)
+        self.estado = "EN_ESTACION" # EN_ESTACION, MOVIMIENTO
+        self.pasajeros = []
+        self.proxima_estacion_id = None
 
-    def __str__(self):
-        return f"Tren {self.id_tren} ({self.modelo})"
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "modelo": self.modelo,
+            "km_actual": self.km_actual,
+            "direccion": self.direccion,
+            "ocupacion": len(self.pasajeros),
+            "capacidad": self.capacidad
+        }
