21d25e51d43ac6499f341709df8f5843f8e1356e
agustinaravenac <agustin.aravena@alumnos.uach.cl>
2025-11-30 22:12:27 -0300

    Update menu_window.py

--- ui/menu_window.py
+++ ui/menu_window.py
@@ -8,33 +8,24 @@ class MenuWindow(tk.Frame):
         super().__init__(parent)
         self.controller = controller
         self.configure(bg=settings.COLOR_FONDO)
-
         label = tk.Label(self, text="Simulador de Tráfico Ferroviario EFE", 
                          font=("Arial", 24, "bold"), bg=settings.COLOR_FONDO, fg=settings.COLOR_TEXTO_MORADO)
         label.pack(pady=50)
-
         btn_nueva = tk.Button(self, text="Nueva Simulación", 
                               command=lambda: controller.show_frame("SimulationWindow"), 
                               width=25, height=2, font=("Arial", 12))
         btn_nueva.pack(pady=10)
-
-        
         def realizar_carga():
             ruta = os.path.join(settings.DATA_DIR, "partida_guardada.json")
-            
             if not os.path.exists(ruta):
                 messagebox.showwarning("Aviso", "No se encontró ningún archivo de guardado.\n\nInicia una nueva simulación y guárdala primero.")
                 return 
-            
             controller.estado_simulacion.cargar_partida()
-            
             controller.show_frame("SimulationWindow")
-
         btn_cargar = tk.Button(self, text="Cargar Simulación", 
                                command=realizar_carga,
                                width=25, height=2, font=("Arial", 12))
         btn_cargar.pack(pady=10)
-
         btn_salir = tk.Button(self, text="Salir", command=parent.quit, 
                               width=25, height=2, font=("Arial", 12), bg="#e74c3c", fg="white")
         btn_salir.pack(pady=10)
